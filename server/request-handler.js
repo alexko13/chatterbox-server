fbc7614dd16d8f586bca55c591604e26374e1600
Alex Ko <alexko@utexas.edu>
2016-02-02 17:15:40 -0700

    clean up header object

--- server/request-handler.js
+++ server/request-handler.js
@@ -1,38 +1,34 @@
 
-var requestHandler = function(request, response) {
+var defaultCorsHeaders = {
+  "access-control-allow-origin": "*",
+  "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "access-control-allow-headers": "content-type, accept",
+  "access-control-max-age": 10, // Seconds.
+  "Content-Type": "application/json"
+};
 
-  var headers = defaultCorsHeaders;
-  headers['Content-Type'] = "application/json";
+exports.requestHandler = function(request, response) {
 
   var URI = /\/classes./;
 
   if(request.method === 'OPTIONS' && URI.test(request.url)) {
-    response.writeHead(200, headers);
+    response.writeHead(200, defaultCorsHeaders);
     response.end();
   } else if(request.method === 'GET' && URI.test(request.url)) {
-    response.writeHead(200, headers);
+    response.writeHead(200, defaultCorsHeaders);
     response.end(JSON.stringify(storage));
   } else if(request.method === 'POST' && URI.test(request.url)) {
-    response.writeHead(201, headers);
+    response.writeHead(201, defaultCorsHeaders);
     request.on('data', function(data) {
       storage.results.unshift(JSON.parse(data));
     });
     response.end(JSON.stringify(storage));
   } else {
-    response.writeHead(404, headers);
+    response.writeHead(404, defaultCorsHeaders);
     response.end();
   }
-};
-
-var defaultCorsHeaders = {
-  "access-control-allow-origin": "*",
-  "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
-  "access-control-allow-headers": "content-type, accept",
-  "access-control-max-age": 10 // Seconds.
 };
 
 var storage = {
   results: []
 };
-
-module.exports.requestHandler = requestHandler;
